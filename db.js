d8d4b9de0e1759c2addec084ff596e5d9b255b1a
MajkellVZ <majkellveizaj@gmail.com>
2020-04-29 23:28:04 +0200

    db

--- db.js
+++ db.js
@@ -2,10 +2,11 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Dejvi18.',
-    database: 'gmd'
+    host: 'db-mysql-nyc3-05703-do-user-7438322-0.a.db.ondigitalocean.com',
+    user: 'doadmin',
+    password: 'fogq7x2apn3wd1ki',
+    database: 'defaultdb',
+    port: '25060'
 });
 
 connection.connect(error => {
